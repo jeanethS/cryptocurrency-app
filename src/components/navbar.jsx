5ce9aaba2864c4668466ca5903b1000dd83bfc3a
Jeaneth Hernandez <jeaneth.hernandez.r@gmail.com>
2022-06-11 12:41:10 -0500

    skeleton

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,56 @@
 import React from "react";
 import { Menu, Typography, Avatar, Button } from "antd";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import icon from "../images/cryptocurrency.png";
-
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 function Navbar() {
+  const [key, setKey] = useState("");
+  const navigate = useNavigate();
+  const menuItems = [
+    {
+      key: "home",
+      icon: <HomeOutlined />,
+      label: "Home",
+      onClick: () => {
+        navigate("/");
+      },
+    },
+    {
+      key: "cryptocurrencies",
+      icon: <FundOutlined />,
+      label: "Cryptocurrencies",
+      onClick: () => {
+        navigate("/cryptocurrencies");
+      },
+    },
+    {
+      key: "exchanges",
+      icon: <MoneyCollectOutlined />,
+      label: "Exchanges",
+      onClick: () => {
+        navigate("/exchanges");
+      },
+    },
+    {
+      key: "news",
+      icon: <BulbOutlined />,
+      label: "News",
+      onClick: () => {
+        navigate("/news");
+      },
+    },
+  ];
+  const handleClick = (e) => {
+    console.log(e);
+    setKey(e.key);
+  };
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -12,6 +59,7 @@ function Navbar() {
           <Link to="/"> Crytoverse </Link>
         </Typography.Title>
       </div>
+      <Menu onClick={handleClick} theme="dark" items={menuItems}></Menu>
     </div>
   );
 }
